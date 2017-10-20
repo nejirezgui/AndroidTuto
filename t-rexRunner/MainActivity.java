fea7024eba3c158efb4958c1e4d03fb701ba0164
nejirezgui <31188124+nejirezgui@users.noreply.github.com>
2017-10-20 17:45:57 +0200

    Create MainActivity.java

---
+++ t-rexRunner/MainActivity.java
@@ -0,0 +1,21 @@
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.inputmethod.InputMethodManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView=(WebView)findViewById(R.id.wb) ;
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("file:///android_asset/index.html");
+
+    }
+}
